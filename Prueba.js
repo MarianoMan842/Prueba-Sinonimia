1fd91968a56f305afaa194ec2a5684365bbb3d36
MarianoMan842 <138440409+MarianoMan842@users.noreply.github.com>
2025-12-07 21:18:13 +0100

    Add files via upload

--- Prueba.js
+++ Prueba.js
@@ -2,11 +2,17 @@
 const sinonimos = ['valia', 'ausencia', 'precio', 'hostil', 'conflicto', 'sencillez'] // No pilla las tildes, eso es un problema
 const sinonimo = sinonimos[Math.floor(Math.random() * sinonimos.length)]
 
-const cont1 = document.getElementById("contenedor")
-const cont2 = document.getElementById("respuestaCorrecta")
+const title = 'SINONIMOS'
+const aclaración = ' (si sale valia es valía)'
+const respuestaCorrecta = 'Respuesta correcta, presiona f5 para que genere otra palabra, si sale la misma dele al f5 otra vez'
+const respuestaIncorrecta = 'Respuesta incorrecta, presiona f5 para que genere otra palabra, si sale la misma dele al f5 otra vez. Si crees que la respuesta que has escrito es válida puede que se deba a que he escrito solamente los sinonimos que salen en la RAE.'
+const teclaEnter = '"ENTER"'
 
-cont1.innerHTML += `<p class="frase">Dime el sinónimo de <span class="variable">${sinonimo}</span> (si sale valia es valía)</p>`
-cont1.innerHTML += `<p class="frase">Presiona la tecla "ENTER" para ingresar tu respuesta</p>`
+const cont1 = document.getElementById("contenedor")
+
+cont1.innerHTML += `<p class="frase"><span class="title">${title}</span></p>`
+cont1.innerHTML += `<p class="frase">Dime el sinónimo de <span class="variable1">${sinonimo}</span><span class="variable2">${aclaración}</span></p>`
+cont1.innerHTML += `<p class="frase">Presiona la tecla <span class="teclaEnter">${teclaEnter}</span> para ingresar tu respuesta<span class="fotoEnter"></span></p>`
 
 if(sinonimo === 'valia'){
 document.addEventListener("keydown", (event) => {
@@ -14,10 +20,10 @@ document.addEventListener("keydown", (event) => {
     const solucionS = prompt ('Ingresa tu respuesta: ')
     const solucionesS = ['valer', 'valor', 'mérito', 'precio', 'estimabilidad', 'preciosidad']
     if(solucionesS.includes(solucionS)){
-         document.getElementById("respuestaCorrecta").innerHTML = 'Respuesta correcta, presiona f5 para que genere otra palabra, si sale la misma dele al f5 otra vez'
+         cont1.innerHTML += `<p class="frase"><span class="respuestaCorrecta">${respuestaCorrecta}</span><span class="fotoTick"></span></p>`
       }
      else {
-         document.getElementById("respuestaCorrecta").innerHTML = 'Respuesta incorrecta, presiona f5 para que genere otra palabra, si sale la misma dele al f5 otra vez. Si crees que la respuesta que has escrito es válida puede que se deba a que he escrito solamente los sinonimos que salen en la RAE.'
+         cont1.innerHTML += `<p class="frase"><span class="respuestaIncorrecta">${respuestaIncorrecta}</span><span class="fotoX"></span></p>`
       }
   }
 })
@@ -29,10 +35,10 @@ document.addEventListener("keydown", (event) => {
     const solucionS = prompt ('Ingresa tu respuesta: ')
     const solucionesS = ['alejamiento', 'separación', 'desaparición', 'marcha', 'partida', 'abandono', 'viaje', 'huida', 'retirada', 'emigración', 'expatriación', 'destierro', 'falta', 'carencia', 'privación', 'vacío', 'insuficiencia', 'escasez', 'omisión', 'defecto', 'laguna', 'inexistencia']
     if(solucionesS.includes(solucionS)){
-         document.getElementById("respuestaCorrecta").innerHTML = 'Respuesta correcta, presiona f5 para que genere otra palabra, si sale la misma dele al f5 otra vez'
+         cont1.innerHTML += `<p class="frase"><span class="respuestaCorrecta">${respuestaCorrecta}</span><span class="fotoTick"></span></p>`
       }
      else {
-         document.getElementById("respuestaCorrecta").innerHTML = 'Respuesta incorrecta, presiona f5 para que genere otra palabra, si sale la misma dele al f5 otra vez. Si crees que la respuesta que has escrito es válida puede que se deba a que he escrito solamente los sinonimos que salen en la RAE.'
+         cont1.innerHTML += `<p class="frase"><span class="respuestaIncorrecta">${respuestaIncorrecta}</span><span class="fotoX"></span></p>`
       }
   }
 })
@@ -44,10 +50,10 @@ document.addEventListener("keydown", (event) => {
     const solucionS = prompt ('Ingresa tu respuesta: ')
     const solucionesS = ['coste', 'costo', 'importe', 'valor', 'valía', 'monto', 'montante', 'tarifa', 'cantidad', 'suma', 'total', 'cuenta', 'estimación', 'tasación']
     if(solucionesS.includes(solucionS)){
-         document.getElementById("respuestaCorrecta").innerHTML = 'Respuesta correcta, presiona f5 para que genere otra palabra, si sale la misma dele al f5 otra vez'
+         cont1.innerHTML += `<p class="frase"><span class="respuestaCorrecta">${respuestaCorrecta}</span><span class="fotoTick"></span></p>`
       }
      else {
-         document.getElementById("respuestaCorrecta").innerHTML = 'Respuesta incorrecta, presiona f5 para que genere otra palabra, si sale la misma dele al f5 otra vez. Si crees que la respuesta que has escrito es válida puede que se deba a que he escrito solamente los sinonimos que salen en la RAE.'
+         cont1.innerHTML += `<p class="frase"><span class="respuestaIncorrecta">${respuestaIncorrecta}</span><span class="fotoX"></span></p>`
       }
   }
 })
@@ -59,10 +65,10 @@ document.addEventListener("keydown", (event) => {
     const solucionS = prompt ('Ingresa tu respuesta: ')
     const solucionesS = ['contrario', 'enemigo', 'adverso', 'desfavorable', 'rival', 'adversario']
     if(solucionesS.includes(solucionS)){
-         document.getElementById("respuestaCorrecta").innerHTML = 'Respuesta correcta, presiona f5 para que genere otra palabra, si sale la misma dele al f5 otra vez'
+         cont1.innerHTML += `<p class="frase"><span class="respuestaCorrecta">${respuestaCorrecta}</span><span class="fotoTick"></span></p>`
       }
      else {
-         document.getElementById("respuestaCorrecta").innerHTML = 'Respuesta incorrecta, presiona f5 para que genere otra palabra, si sale la misma dele al f5 otra vez. Si crees que la respuesta que has escrito es válida puede que se deba a que he escrito solamente los sinonimos que salen en la RAE.'
+         cont1.innerHTML += `<p class="frase"><span class="respuestaIncorrecta">${respuestaIncorrecta}</span><span class="fotoX"></span></p>`
       }
   }
 })
@@ -74,10 +80,10 @@ document.addEventListener("keydown", (event) => {
     const solucionS = prompt ('Ingresa tu respuesta: ')
     const solucionesS = ['guerra','combate', 'lucha', 'pelea', 'enfrentamiento', 'disputa', 'pugna', 'lid', 'colisión', 'choque', 'problema', 'dificultad', 'apuro', 'aprieto', 'compromiso', 'ahogo', 'brete']
     if(solucionesS.includes(solucionS)){
-         document.getElementById("respuestaCorrecta").innerHTML = 'Respuesta correcta, presiona f5 para que genere otra palabra, si sale la misma dele al f5 otra vez'
+         cont1.innerHTML += `<p class="frase"><span class="respuestaCorrecta">${respuestaCorrecta}</span><span class="fotoTick"></span></p>`
       }
      else {
-         document.getElementById("respuestaCorrecta").innerHTML = 'Respuesta incorrecta, presiona f5 para que genere otra palabra, si sale la misma dele al f5 otra vez. Si crees que la respuesta que has escrito es válida puede que se deba a que he escrito solamente los sinonimos que salen en la RAE.'
+         cont1.innerHTML += `<p class="frase"><span class="respuestaIncorrecta">${respuestaIncorrecta}</span><span class="fotoX"></span></p>`
       }
   }
 })
@@ -89,10 +95,10 @@ document.addEventListener("keydown", (event) => {
     const solucionS = prompt ('Ingresa tu respuesta: ')
     const solucionesS = ['facilidad', 'simplicidad', 'asequilibidad', 'elementalidad', 'factibilidad', 'naturalidad', 'llaneza', 'campenchanía', 'espontaneidad', 'sobriedad', 'austeridad', 'discreción']
     if(solucionesS.includes(solucionS)){
-         document.getElementById("respuestaCorrecta").innerHTML = 'Respuesta correcta, presiona f5 para que genere otra palabra, si sale la misma dele al f5 otra vez'
+         cont1.innerHTML += `<p class="frase"><span class="respuestaCorrecta">${respuestaCorrecta}</span><span class="fotoTick"></span></p>`
       }
      else {
-         document.getElementById("respuestaCorrecta").innerHTML = 'Respuesta incorrecta, presiona f5 para que genere otra palabra, si sale la misma dele al f5 otra vez. Si crees que la respuesta que has escrito es válida puede que se deba a que he escrito solamente los sinonimos que salen en la RAE.'
+         cont1.innerHTML += `<p class="frase"><span class="respuestaIncorrecta">${respuestaIncorrecta}</span><span class="fotoX"></span></p>`
       }
   }
 })
@@ -104,10 +110,10 @@ document.addEventListener("keydown", (event) => {
     const solucionS = prompt ('Ingresa tu respuesta: ')
     const solucionesS = ['aversión', 'animadversión', 'tirria', 'antipatía', 'ojeriza', 'manía', 'animosidad', 'odio', 'rabia', 'enemistad', 'rencor']
     if(solucionesS.includes(solucionS)){
-         document.getElementById("respuestaCorrecta").innerHTML = 'Respuesta correcta, presiona f5 para que genere otra palabra, si sale la misma dele al f5 otra vez'
+         cont1.innerHTML += `<p class="frase"><span class="respuestaCorrecta">${respuestaCorrecta}</span><span class="fotoTick"></span></p>`
       }
      else {
-         document.getElementById("respuestaCorrecta").innerHTML = 'Respuesta incorrecta, presiona f5 para que genere otra palabra, si sale la misma dele al f5 otra vez. Si crees que la respuesta que has escrito es válida puede que se deba a que he escrito solamente los sinonimos que salen en la RAE.'
+         cont1.innerHTML += `<p class="frase"><span class="respuestaIncorrecta">${respuestaIncorrecta}</span><span class="fotoX"></span></p>`
       }
   }
 })
